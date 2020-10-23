6bfd2db12ba314f81c4d66357c492b16c8f5e339
Tim Claes <tim.claes@me.com>
2020-10-23 20:54:14 +0200

    adding whitenoise for static file management

--- hh_modules/settings/base.py
+++ hh_modules/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,6 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 TATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
--- hh_modules/settings/prod.py
+++ hh_modules/settings/prod.py
@@ -5,3 +5,5 @@ SECRET_KEY = 'f=sxf551h(qt*-e7@b&2x@r$6d2&bf#qt#nza7f@e10a)5j^3v'
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
