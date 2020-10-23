8880daec3db54d2bf36b624d89b984239e8d3135
Tim Claes <tim.claes@me.com>
2020-10-23 21:18:33 +0200

    updating allowed hosts

--- hh_modules/settings/prod.py
+++ hh_modules/settings/prod.py
@@ -3,8 +3,10 @@ from .base import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-import dj_database_url
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.aws.amazon.com', '.herokuapp.com']
 
+
+import dj_database_url
 DATABASES = { 'default': dj_database_url.config() }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
