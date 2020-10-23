14a699c723470f8fae983ffa55701e9b5b33e300
Tim Claes <tim.claes@me.com>
2020-10-23 20:26:25 +0200

    setting up heroku

--- juxtapose/models.py
+++ juxtapose/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class JuxtaposeData(models.Model):
+
+  image1 = models.ImageField(upload_to='images/juxtapose')
+  image2 = models.ImageField(upload_to='images/juxtapose')
