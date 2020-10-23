d0d3f555fcfaac38559adc21727cc32d233622b2
Tim Claes <tim.claes@me.com>
2020-10-23 21:03:17 +0200

    changing syntax

--- hh_modules/settings/prod.py
+++ hh_modules/settings/prod.py
@@ -4,6 +4,7 @@ from .base import *
 SECRET_KEY = 'f=sxf551h(qt*-e7@b&2x@r$6d2&bf#qt#nza7f@e10a)5j^3v'
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASES = { 'default': dj_database_url.config() }
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
