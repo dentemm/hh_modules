292300f93d3eb2ac80187a158e18832a119bee62
Tim Claes <tim.claes@me.com>
2020-10-24 19:44:50 +0200

    getting to know new django structure

--- juxtapose/admin.py
+++ juxtapose/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import JuxtaposeData
+
+class JuxtaposeAdmin(admin.ModelAdmin):
+  fields = ['image1', 'image2']
+
+admin.site.register(JuxtaposeData, JuxtaposeAdmin)
