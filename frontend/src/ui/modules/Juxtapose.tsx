d862a524b4887ff04d8a1ed945ed7444f6a22444
Tim Claes <tim.claes@me.com>
2020-10-25 13:07:47 +0100

    saving changes

--- frontend/src/ui/modules/Juxtapose.tsx
+++ frontend/src/ui/modules/Juxtapose.tsx
@@ -5,14 +5,19 @@ import after from '../../static/dev/images/after.jpg'
 
 const JuxtaPose: React.FC = () => {
   return (
-    <div>
+    <div
+      className="d-flex bg-primary"
+    >
       <img
         src={before}
         alt={''}
+        className="position-relative"
       />
       <img
         src={after}
         alt={''}
+        className="position-absolute"
+        style={{opacity: 0.5}}
       />
     </div>
   )
