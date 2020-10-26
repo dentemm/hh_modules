5a0c461e29fe989b1855df9969a962e51b228780
Tim Claes <tim.claes@me.com>
2020-10-26 21:04:41 +0100

    first working draft for juxtapose

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,17 +10,6 @@ function App() {
   return (
     <div className="App">
       <Juxtapose />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
     </div>
   );
 }
--- frontend/src/ui/modules/Juxtapose.tsx
+++ frontend/src/ui/modules/Juxtapose.tsx
@@ -1,53 +1,70 @@
 import * as React from 'react'
 
+import { useDrag, useMove } from 'react-use-gesture'
+
 import before from '../../static/dev/images/before.jpg'
 import after from '../../static/dev/images/after.jpg'
+import { FullGestureState } from 'react-use-gesture/dist/types'
 
 const JuxtaPose: React.FC = () => {
+
+  const ref = React.useRef<HTMLDivElement>(null)
+  const [dimensions, setDimensions] = React.useState({width: 0, height: 0}) 
+  const [width, setWidth] = React.useState(50)
+
+  React.useEffect(() => {
+    if (ref.current) {
+      setDimensions({
+        width: ref.current.offsetWidth,
+        height: ref.current.offsetHeight
+      })
+    }
+  }, [])
+
+  const gesture = useDrag(state => handler(state))
+
+  const handler = (state: FullGestureState<'drag'>) => {
+
+    const currentValue = state.xy[0]
+    let percentage = 100 * currentValue / dimensions.width
+
+    if (percentage > 100) {
+      percentage = 100
+    } else if (percentage < 0) {
+      percentage = 0
+    }
+
+    console.log(percentage)
+
+    setWidth(percentage)
+  }
+
   return (
-    <div
-      className="d-flex justify-content-center"
+    <div 
+      ref={ref}
+      {...gesture()}
     >
-      <div 
-        style={{width: '50%'}}
-        className="d-flex bg-secondary d-block"
+      <figure
+        style={{width: '100%'}}
       >
-        <figure
-          className="bg-primary"
-          style={{width: '100%'}}
+        <img
+          src={before}
+          alt={''}
+          className="d-block vw-100"
+          // style={{width: '100%'}}
+        />
+        <figcaption
+          className="position-absolute overflow-hidden" 
+          style={{top: 0, left: 0, width: `${width}%`}}
         >
           <img
-            src={before}
+            src={after}
             alt={''}
-            className="position-relative img-fluid"
-            style={{width: '100%'}}
+            className="d-block vw-100"
+            style={{opacity: '100%'}}
           />
-          <figcaption
-            className="position-absolute"
-            style={{top: 0, left: '25%', width: '50%'}}
-          >
-            <img
-              src={after}
-              alt={''}
-              className="img-fluid"
-              style={{width: '100%'}}
-            />
-          </figcaption>
-          
-        </figure>
-      {/* <img
-        src={before}
-        alt={''}
-        className="position-relative img-fluid"
-        // style={{width: '100%'}}
-      />
-      <img
-        src={after}
-        alt={''}
-        className="position-absolute"
-        // style={{top: 0, left: 0}}
-      /> */}
-      </div>
+        </figcaption>
+      </figure>
     </div>
   )
 }
