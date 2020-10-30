503b110f2169becb566f3deae14321d623ff9d06
Tim Claes <tim.claes@me.com>
2020-10-30 12:21:39 +0100

    added script to generate predictable build

--- hh_modules/urls.py
+++ hh_modules/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from .views import HomeView
 
@@ -22,4 +24,5 @@ urlpatterns = [
     # path('juxtapose/', include('juxtapose.urls')),
     path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
