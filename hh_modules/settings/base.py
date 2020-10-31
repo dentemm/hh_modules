2b1191ddc39b7a0f95f55989ca9b9d7dfb384cf9
Tim Claes <tim.claes@me.com>
2020-10-31 18:37:13 +0100

    removing references to build folder

--- hh_modules/settings/base.py
+++ hh_modules/settings/base.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, 'frontend', 'build'),
 ]
 
-print(os.path.join(BASE_DIR, 'frontend', 'build', 'static'))
+# print(os.path.join(BASE_DIR, 'frontend', 'build', 'static'))
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
